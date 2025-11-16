2c3bb362c55e9af46a5853597704008e4ddffff6
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2025-11-16 12:32:58 +0100

    weather_advice_task_updated

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,13 +3,13 @@
 2. provide clothing recommendations
 3. provide a statement that handles unexpected input
 """
-user_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if user_weather == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif user_weather == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif user_weather == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I dont have recommendations for this weather.")
