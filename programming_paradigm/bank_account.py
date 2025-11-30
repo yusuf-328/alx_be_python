f01f399d7c0377ae0e7868cb511c3dcf5782ce76
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2025-11-30 10:13:34 +0100

    add programming_paradigms

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,7 +14,7 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
           
        
 
