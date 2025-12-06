c2d0b03e41297cba8f9c87ba7de863e44cdc1c54
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2025-12-06 22:23:46 +0100

    add polumorphism_demo.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -25,7 +25,8 @@ class PrintBook(Book):
 
 class Library:
     def __init__(self):
-        self.books = []        
+        self.books = []  
+              
     def add_book(self, book):
         self.books.append(book)
 
---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,20 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.width = width
+        self.length = length
+
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def area(self):
+        return math.pi * (self.radius**2)
