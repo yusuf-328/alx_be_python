e0a2cbbeb773713ba2ce8c20becc6c1f4a753b57
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2025-12-06 23:10:03 +0100

    add class_static_methods.py

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,17 @@
+class Calculator:
+    calculation_type = "Arithmetric Operations"
+    @staticmethod
+    def add(a,b):
+        return a + b
+    
+    @classmethod
+    def multiply(cls, a, b,):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
+        
+        
+
+  
+
+
+
--- oop/main.py
+++ oop/main.py
@@ -41,17 +41,33 @@
 
 
 
-from polymorphism_demo import Shape, Rectangle, Circle
-import math
+# from polymorphism_demo import Shape, Rectangle, Circle
+# import math
+
+# def main():
+#     shapes = [
+#         Rectangle(10, 5),
+#         Circle(7)
+#     ]
+
+#     for shape in shapes:
+#         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+from class_static_methods_demo import Calculator
 
 def main():
-    shapes = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
 
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__":
     main()
